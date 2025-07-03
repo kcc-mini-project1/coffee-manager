f40e8303bfb4fc896608b60bad447ef0613c4582
김이슬 <kdewly715@gmail.com>
2025-07-03 17:39:21 +0900

    Feat : VO 파일 추가

---
+++ MiniProject/src/Employee.java
@@ -0,0 +1,12 @@
+import java.sql.Date;
+import lombok.Data;
+
+@Data
+public class Employee {
+	private int employeeId;
+	private String name;
+	private String phoneNumber;
+	private String title;
+	private int salary;
+	private int managerId;
+}
---
+++ MiniProject/src/Member.java
@@ -0,0 +1,10 @@
+import java.sql.Date;
+
+import lombok.Data;
+
+@Data
+public class Member{
+	private String customerId;
+	private int stamp;
+	private int coupon;
+}
